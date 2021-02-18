89fcb7d78e58fc1982e4c028c45bddcf0a4d8064
Carlos H <carlos.e.herrera.j@gmail.com>
2021-02-18 15:54:23 -0400

    Sync - 18/02/2021 - 03:54 pm
    Sync - 18/02/2021 - 03:54 pm

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Swal from "sweetalert2";
 import shortid from "shortid";
 
 function App() {
+
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
+  const successColor = "#53a451";
+  const warningColor = "#f7c244";
 
   const addTask = (e) => {
     e.preventDefault();
@@ -42,6 +46,27 @@ function App() {
     );
   };
 
+  const deleteTask = (id) =>{
+    const tasksFilter = tasks.filter(task => task.id !== id);
+    setTasks(tasksFilter);
+  }
+
+  const taskDeleteShowModal = (task) => {
+    const swalProperty = {
+      icon: 'warning',
+      title: 'Are you shure delete this?',
+      html: 'Task: <b>' + task.name + '</b>',
+      timer: null,
+      confirmButtonColor: warningColor,
+      confirmButtonText: 'Yes',
+      showCancelButton: true,
+      cancelButtonColor: successColor,
+      cancelButtonText: 'No',
+    };
+
+    Swal.fire(swalProperty);
+  }
+
   const sendMessage = (msgType, msgTitle, msgText, timer, confirmButton) => {
     const swalProperty = {
       icon: msgType,
@@ -57,6 +82,7 @@ function App() {
   return (
     <div className="container mt-5">
       <h1>Tasks</h1>
+      <hr/>
 
       <div className="row">
         <div className="col-md-8">
@@ -68,7 +94,8 @@ function App() {
 
               <li className="list-group-item" key={task.id}>
                 <span className="lead"><i className="text-success far fa-check-circle"></i> {task.name}</span>
-                <a className="btn btn-outline-danger btn-sm float-right mx-2">
+                <a className="btn btn-outline-danger btn-sm float-right mx-2"
+                  onClick={() => taskDeleteShowModal(task)}>
                   <i className="far fa-trash-alt"></i>
                 </a>
                 <a className="btn btn-outline-warning btn-sm float-right">
